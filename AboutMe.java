bd13e70ec84f68bf7a41a23c82c6217397dede9f
melique156 <melique.williams156@sccdsb.net>
2020-09-22 14:09:56 -0400

    AboutMe

--- AboutMe.java
+++ AboutMe.java
@@ -1,24 +1,32 @@
 /*
- * Programmer:
+ * Programmer: Melique.Williams
  * Date:
  * Purpose:
  */
 
 public class AboutMe {
     public static void main (String [] args) {
-        // Declaring the variables
-        // TODO: Write the code the declares the variables
-
+        // All variables
+    	
+    	final string myName = Melique.Williams
+        int myAge = 13
+        double myHeightInMeteres = 1.73736
+        
         // Print the values
         System.out.println("Hi, my name is " + myName);
         System.out.println("I am " + myAge + " years old.");
         System.out.println("My height is " + myHeightInMetres + " m.");
 
         // Change the variables
-        // TODO: Write the code that changes the variables
-
+        // Write the code that changes the variables
+        myAge = 16
+        myHeightInMeteres = 1.88976
+        
         // Print the new values
-        System.out.println("Some time passes...");
+         System.out.println("Some time passes...");
+        
         // TODO: Write the last two print statements
+        System.out.println("I am now " + myAge + " years old.");
+        System.out.println("My height is " + myHeightInMetres + " m.");
     }
 }
