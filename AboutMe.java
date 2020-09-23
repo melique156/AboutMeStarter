d3ccdb36c07fc077a8c259efc289cb0bdf9df74f
melique156 <melique.williams156@sccdsb.net>
2020-09-23 08:39:16 -0400

    changed height

--- AboutMe.java
+++ AboutMe.java
@@ -9,27 +9,27 @@
         // All variables
 		String myName = "Melique.W.";
         int myAge = 13;
-        double myHeightInMeteres  = 1.73;
+        double myHeightInMetres  = 1.73;
         
         //Print the values
         System.out.println("Hi, my name is " + myName);
         System.out.println("I am " + myAge + " years old.");
-        System.out.println("My height is " + myHeightInMeteres + " m.");
+        System.out.println("My height is " + myHeightInMetres + " m.");
         
         //Change the variables
         myAge = 16;
-        myHeightInMeteres = 1.88976;
+        myHeightInMetres = 1.88;
         
       //Write the code that changes the variables
       int myAge1 = 16;
-      double myHeightInMeteres1 = 1.88;
+      double myHeightInMetres1 = 1.88;
         
         //Print the new values
          System.out.println("Some time passes...");
         
         // Write the last two print statements
         System.out.println("I am now " + myAge1 + " years old.");
-        System.out.println("I have grown and am " + myHeightInMeteres1 + " m tall.");
+        System.out.println("I have grown and am " + myHeightInMetres1 + " m tall.");
     }
 }
 
