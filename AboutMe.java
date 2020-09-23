b79891ae2853a3eae6e8704d302ac79a538683af
melique156 <melique.williams156@sccdsb.net>
2020-09-23 08:54:09 -0400

    Revert "new change in h"
    This reverts commit 9e10b6ae8607c63c9c0d4101dc16f16f33d01ba3.

--- AboutMe.java
+++ AboutMe.java
@@ -9,35 +9,27 @@
         // All variables
 		String myName = "Melique.W.";
         int myAge = 13;
-        double myHeightInMeteres  = 1.73;
+        double myHeightInMetres  = 1.73;
         
         //Print the values
         System.out.println("Hi, my name is " + myName);
         System.out.println("I am " + myAge + " years old.");
-        System.out.println("My height is " + myHeightInMeteres + " m.");
+        System.out.println("My height is " + myHeightInMetres + " m.");
         
         //Change the variables
         myAge = 16;
-        myHeightInMeteres = 1.88976;
+        myHeightInMetres = 1.88;
         
       //Write the code that changes the variables
       int myAge1 = 16;
-<<<<<<< HEAD
       double myHeightInMetres2 = 1.88;
-=======
-      double myHeightInMeteres1 = 1.88;
->>>>>>> parent of d3ccdb3... changed height
         
         //Print the new values
          System.out.println("Some time passes...");
         
         // Write the last two print statements
         System.out.println("I am now " + myAge1 + " years old.");
-<<<<<<< HEAD
         System.out.println("I have grown and am " + myHeightInMetres2 + " m tall.");
-=======
-        System.out.println("I have grown and am " + myHeightInMeteres1 + " m tall.");
->>>>>>> parent of d3ccdb3... changed height
     }
 }
 
