7b753f9cf79b9bb79f014247c09657fd4e962a80
melique156 <melique.williams156@sccdsb.net>
2020-09-23 07:52:45 -0400

    date changed

--- AboutMe.java
+++ AboutMe.java
@@ -1,31 +1,30 @@
 /*
  * Programmer: Melique.Williams
- * Date:
- * Purpose:
+ * Date:Sept 23,2020
+ * Purpose: To find out my age and height throught the years.
  */
 
 public class AboutMe {
     public static void main (String [] args) {
         // All variables
-    	
     	final string myName = Melique.Williams
         int myAge = 13
         double myHeightInMeteres = 1.73736
         
-        // Print the values
+        //Print the values
         System.out.println("Hi, my name is " + myName);
         System.out.println("I am " + myAge + " years old.");
         System.out.println("My height is " + myHeightInMetres + " m.");
 
-        // Change the variables
-        // Write the code that changes the variables
+        //Change the variables
+        //Write the code that changes the variables
         myAge = 16
         myHeightInMeteres = 1.88976
         
-        // Print the new values
+        //Print the new values
          System.out.println("Some time passes...");
         
-        // TODO: Write the last two print statements
+        // Write the last two print statements
         System.out.println("I am now " + myAge + " years old.");
         System.out.println("My height is " + myHeightInMetres + " m.");
     }
